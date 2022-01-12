15ce83db60e3545b9981c3931704bd4243ee236d
Mike Starr <imhs@protonmail.com>
2022-01-12 06:38:29 -0800

    Assignment completed except for 3rd test

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = "BOB";
+}
+
+function setBestCustomer() {
+  var bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(text) {
+  bestCustomer = text;
+}
+
+const leastFavoriteCustomer = "mike";
+function changeLeastFavoriteCustomer(input) {
+  leastFavoriteCustomer = input;
+}
